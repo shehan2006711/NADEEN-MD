3c506d787d63f1408826ddfbc22a1aed81f61994
shehan2006711 <154323632+shehan2006711@users.noreply.github.com>
2025-02-10 22:13:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "yAxzkbYJ#cC3X9K5TR7s3s5a6VkgxPvgQBBd_mPtR78mh61B-Nio", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true", //true or false
-STATUES_REPLY: process.env.STATUES_REPLY || "true", //true or false
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false", //true or false
+STATUES_REPLY: process.env.STATUES_REPLY || "false", //true or false
 AUTO_REACT: process.env.AUTO_REACT || "false", //true or false
 INBOX_BLOCK: process.env.INBOX_BLOCK || "false", //true or false
 AUTO_TYPING: process.env.AUTO_TYPING || "false", //true or false
